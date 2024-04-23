fc4c9780afb87ceba1ebc0232e50d86e41edf672
Andrii0302 <144677052+Andrii0302@users.noreply.github.com>
2024-04-23 22:21:08 +0300

    Create Ksiazka.h

---
+++ Ksiazka.h
@@ -0,0 +1,21 @@
+// Ksiazka.h
+#ifndef KSIAZKA_H
+#define KSIAZKA_H
+#include "EBiblioteka.h"
+
+class Ksiazka {
+private:
+    std::string Autor;
+    std::string Tytul;
+    ElementBiblioteczny* NrID; // Use a pointer here
+    bool Dostepnosc;
+
+    bool CzyDostepna();
+
+public:
+    Ksiazka();
+    Ksiazka(std::string autor, std::string tytul, int id, bool dostepnosc);
+    void WypiszDane();
+};
+
+#endif
