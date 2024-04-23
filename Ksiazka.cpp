ebc797a1a30c7d41443e6ffb1e206773fdaea20d
Andrii0302 <144677052+Andrii0302@users.noreply.github.com>
2024-04-23 22:22:19 +0300

    Update Ksiazka.cpp

--- Ksiazka.cpp
+++ Ksiazka.cpp
@@ -6,7 +6,7 @@ Ksiazka::Ksiazka() {}
 
 Ksiazka::Ksiazka(std::string autor, std::string tytul, int id, bool dostepnosc) 
     : Autor(autor), Tytul(tytul), Dostepnosc(dostepnosc) {
-    NrID = new ElementBiblioteczny(id); // Create a new ElementBiblioteczny object and store its pointer
+    NrID = new ElementBiblioteczny(id); 
 }
 
 bool Ksiazka::CzyDostepna() {
